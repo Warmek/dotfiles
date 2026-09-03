06087c0f0203713edea4154d2cd4ee31cfed0246
Bartosz Stolarczyk <bars_k@vd.dk>
2026-09-03 10:50:58 +0200

    Update roslyn settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -346,11 +346,11 @@ end
 vim.lsp.config('roslyn', {
   settings = {
     ['csharp|background_analysis'] = {
-      dotnet_analyzer_diagnostics_scope = 'fullSolution',
+      dotnet_analyzer_diagnostics_scope = 'openFiles',
       dotnet_compiler_diagnostics_scope = 'fullSolution',
     },
     ['csharp|code_lens'] = {
-      dotnet_enable_references_code_lens = true,
+      dotnet_enable_references_code_lens = false,
     },
     ['csharp|completion'] = {
       dotnet_show_name_completion_suggestions = true,
